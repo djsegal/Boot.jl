dec9b4a72eebb2f8b9def786542b7e22ad2b4013
djsegal <dansegal2@gmail.com>
2017-12-10 17:24:03 -0500

    Use actual boot loading while loading test files

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,12 +8,6 @@ using Suppressor
 
 @testset ExtendedTestSet "All Tests" begin
 
-  include("core/simple.jl")
-  include("core/nested.jl")
-
-  include("core/dependency.jl")
-  include("core/cyclic.jl")
-
-  include("core/documented.jl")
+  include("core/index.jl")
 
 end
