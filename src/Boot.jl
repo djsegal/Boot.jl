d1c805db7bdd7d83ff3680eba896a3a2edf80f55
djsegal <dansegal2@gmail.com>
2017-12-14 06:04:03 -0500

    Remove get_all_symbols function from the project

--- src/Boot.jl
+++ src/Boot.jl
@@ -6,20 +6,15 @@ module Boot
   using Compat
 
   include("strip_output_color.jl")
-
-  include("get_all_symbols.jl")
-  include("get_all_files.jl")
-
   include("attempt_file_load.jl")
   include("clean_shard.jl")
 
   include("include_folder.jl")
+  include("get_all_files.jl")
   include("parse_file.jl")
 
-  export get_all_symbols
-  export get_all_files
-
   export include_folder
+  export get_all_files
   export parse_file
 
 end
