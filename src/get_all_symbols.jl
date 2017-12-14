9358dbc94c5934fc91601399a6b30bd54be85995
djsegal <dansegal2@gmail.com>
2017-12-14 05:41:49 -0500

    Be specific in get_all_symbols file about names()

--- src/get_all_symbols.jl
+++ src/get_all_symbols.jl
@@ -1,7 +1,7 @@
 function get_all_symbols(cur_package::Module)
   all_symbols = []
 
-  for cur_name in names(cur_package, true)
+  for cur_name in cur_package.names(cur_package, true)
     Base.isidentifier(cur_name) || continue
     in(cur_name, (Symbol(cur_package), :eval)) && continue
 
