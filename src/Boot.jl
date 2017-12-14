a430e2a9cafa3b864c8514f99f1430c206cd843c
djsegal <dansegal2@gmail.com>
2017-12-14 05:41:27 -0500

    Remove cruft from project (hacky delete methods)

--- src/Boot.jl
+++ src/Boot.jl
@@ -5,26 +5,19 @@ module Boot
   using Revise
   using Compat
 
-  include("flatten_array.jl")
-
-  include("delete_method.jl")
-  include("delete_docstring.jl")
-  include("purge_corrupted_data.jl")
-
   include("strip_output_color.jl")
 
-  include("get_all_files.jl")
   include("get_all_symbols.jl")
-  include("get_package_methods.jl")
+  include("get_all_files.jl")
+
   include("attempt_file_load.jl")
   include("clean_shard.jl")
 
   include("include_folder.jl")
   include("parse_file.jl")
 
-  export get_all_files
   export get_all_symbols
-  export get_package_methods
+  export get_all_files
 
   export include_folder
   export parse_file
