d5b2e7e54ba1ee8b9c69aa3de0cafb2ce293b7fb
djsegal <dansegal2@gmail.com>
2017-12-22 23:53:44 -0500

    Fix bug where get_all_files relied on abspath

--- src/get_all_files.jl
+++ src/get_all_files.jl
@@ -1,4 +1,8 @@
 function get_all_files(cur_folder::AbstractString; is_sorted::Bool=true, except_for::AbstractArray=[])
+  cur_folder = abspath(cur_folder)
+
+  _expand_except_for!(cur_folder, except_for)
+
   nested_files = _get_nested_files(cur_folder)
 
   is_sorted ? sort!(nested_files) : shuffle!(nested_files)
@@ -6,6 +10,15 @@ function get_all_files(cur_folder::AbstractString; is_sorted::Bool=true, except_
   nested_files = _remove_unwanted_files(nested_files, except_for)
 
   nested_files
+end
+
+function _expand_except_for!(cur_folder::AbstractString, except_for::AbstractArray)
+  for (cur_index, cur_except) in enumerate(except_for)
+    startswith(cur_except, "/") && continue
+
+    except_for[cur_index] =
+      joinpath(cur_folder, cur_except)
+  end
 end
 
 function _get_nested_files(cur_item::AbstractString)
@@ -30,10 +43,6 @@ function _get_nested_files(cur_item::AbstractString)
 end
 
 function _remove_unwanted_files(nested_files::AbstractArray, except_for::AbstractArray)
-  nested_files = map(abspath, nested_files)
-
-  except_for = map(abspath, except_for)
-
   except_files = filter(cur_except -> endswith(cur_except, ".jl"), except_for)
 
   except_folders = filter(cur_except -> endswith(cur_except, "/*"), except_for)
